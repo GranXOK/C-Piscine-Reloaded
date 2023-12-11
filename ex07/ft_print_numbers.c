8904576596beb4875745a2f7c64e5393de2b3842
kazpeiti <kazpeiti@DESKTOP-2CH8H84.localdomain>
2023-12-11 16:49:27 +0100

    Subida de los ejercicios ex06 y ex07

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+void ft_print_numbers(void);
+
+int main(void)
+{
+	ft_print_numbers();
+	return (0);
+}
+
+void ft_print_numbers(void)
+{
+	write(1, "0123456789", 10);
+	write(1, "\n", 1);
+}
